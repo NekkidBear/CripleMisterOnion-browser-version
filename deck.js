50eee152af09812442550efffd18c5fa9457dde9
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-03-07 17:01:04 -0600

    add deck.js with pseudocode comments

---
+++ deck.js
@@ -0,0 +1,20 @@
+/**
+ * This module creates a deck of cards for Cripple Mr. Onion. It differs from the standard 52-card deck
+ * in several ways. 
+ *  There are 8 suits (In this game I used the German and Spanish suits because that is what the
+ *      free spritesheet came with.)
+ *  The deck also contains alternative cards for wilds and jokers.
+ */
+
+const SUITS  = ["Coins","Staves", "Cups", "Swords", "Shields", "Acorns", "Hearts", "Leaves"];
+const RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"];
+
+class card {
+
+}
+
+class deck {
+    /** Each Deck contains 104 cards. Each card has a Suit, a Rank, a sprite and a value. The value can fluctuate depending on the round and modifiers
+ * in effect, but in general, Aces are 1, 2-9 are face value, and Jack, Queen, King are worth 10.
+*/
+}
