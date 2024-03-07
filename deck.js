f104db84b3e73d18e9f3c6f50eea0f5b2717b27e
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-03-07 17:26:39 -0600

    adding deck logic to code, linking scripts and styles to index.html

--- deck.js
+++ deck.js
@@ -9,12 +9,40 @@
 const SUITS  = ["Coins","Staves", "Cups", "Swords", "Shields", "Acorns", "Hearts", "Leaves"];
 const RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"];
 
-class card {
-
+class Card {
+    constructor(){
+        this.rank ="";
+        this.suit ="";
+        this.base_value = 0;
+        this.modified_value = this.base_value;
+    }
 }
 
-class deck {
+class Deck {
     /** Each Deck contains 104 cards. Each card has a Suit, a Rank, a sprite and a value. The value can fluctuate depending on the round and modifiers
  * in effect, but in general, Aces are 1, 2-9 are face value, and Jack, Queen, King are worth 10.
 */
+    constructor(){    
+        this.deck = [];
+        this.shuffle();
+        this.reset();
+        
+        for (let suit in SUITS){
+            for(let rank in RANKS){
+                switch (rank){
+                    case "Ace":
+                        base_value = 1;
+                        break;
+                    case "Jack", "Queen", "King":
+                        base_value = 10;
+                        break;
+                    default: 
+                        base_value = Number(rank);
+                };
+                console.log (rank, suit, base_value);
+                this.deck.push(Card.rank, Card.suit, Card.base_value);
+
+            }
+        }
+    }
 }
